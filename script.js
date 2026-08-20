aacff55120ffdfe9f5b7a66ace58a4ab7480fadf
leonapacanis725-web <leonapacanis725@gmail.com>
2026-08-20 18:12:54 -0400

    Create script.js
    Better Hacker website interactions

---
+++ script.js
@@ -0,0 +1,5 @@
+const startButton = document.querySelector("button");
+
+startButton.addEventListener("click", function () {
+  alert("Welcome to Better Hacker! Your cybersecurity journey starts here.");
+});
